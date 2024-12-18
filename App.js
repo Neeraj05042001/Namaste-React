71ede88cc4a963613806ac26e4f844f4218807a4
Neeraj <neeraj05042001@gmail.com>
2024-12-19 03:23:08 +0530

    Episode - 01

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div", { id: "parent" }, 
+    [React.createElement("div", { id: "child1" },
+        [React.createElement("h1", {id: "heading1"}, "Heading1" ), 
+            React.createElement("h2", {id: "heading1"}, "Heading1" )]), 
+            React.createElement("div", { id: "child1" },
+                [React.createElement("h1", {id: "heading1"}, "Heading1" ), 
+                    React.createElement("h2", {id: "heading1"}, "Heading1" )])
+        ]);
+
+
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(parent)
